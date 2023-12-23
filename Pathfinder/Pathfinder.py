d220118ca34137d1226745e71947318166fd2cec
momadAB <mabaqer99@gmail.com>
2023-12-23 14:54:38 -0500

    Function naming conventions fixes

--- Pathfinder/Pathfinder.py
+++ Pathfinder/Pathfinder.py
@@ -20,7 +20,7 @@ windowWidth = 1200
 
 
 # Calculates the overall cost
-def getDistance(node1, node2):
+def get_distance(node1, node2):
     y_diff = abs(node1.x - node2.x)
     x_diff = abs(node1.y - node2.y)
 
@@ -51,16 +51,16 @@ class VisualNode:
         self.hCost = 0
         self.gCost = 0
 
-    def getFCost(self):
+    def get_f_cost(self):
         return self.hCost + self.gCost
 
-    def getPosition(self):
+    def get_position(self):
         return self.x, self.y
 
-    def setColor(self, color):
+    def set_color(self, color):
         self.color = color
 
-    def checkState(self):
+    def check_state(self):
 
         if self.color == DARK_GREEN or self.color == GREEN or self.color == WHITE or self.color == MAROON:
             return "walkable"
@@ -79,7 +79,7 @@ class VisualNode:
                                      (self.y * self.width, self.x * self.height, self.width - 2, self.width - 2))
         pygame.display.update(self.rect)
 
-    def drawWithAnimationLonger(self, win):
+    def draw_with_longer_animation(self, win):
         i = 1
         while i < self.width - 1:
             for e in pygame.event.get():
@@ -90,7 +90,7 @@ class VisualNode:
             i += 1
             time.sleep(0.0001)
 
-    def getNeighbors(self, grid):
+    def get_neighbors(self, grid):
         self.straight_neighbors = []
         self.diagonal_neighbors = []
         rightOpen = False
@@ -100,22 +100,22 @@ class VisualNode:
 
         # Straight Neighbors
         if self.y != GRID_X - 1:
-            if grid[self.x][self.y + 1].checkState() != "barrier":  # Right
+            if grid[self.x][self.y + 1].check_state() != "barrier":  # Right
                 self.straight_neighbors.append(grid[self.x][self.y + 1])
                 rightOpen = True
 
         if self.y != 0:
-            if grid[self.x][self.y - 1].checkState() != "barrier":  # Left
+            if grid[self.x][self.y - 1].check_state() != "barrier":  # Left
                 self.straight_neighbors.append(grid[self.x][self.y - 1])
                 leftOpen = True
 
         if self.x != GRID_Y - 1:
-            if grid[self.x + 1][self.y].checkState() != "barrier":  # Down
+            if grid[self.x + 1][self.y].check_state() != "barrier":  # Down
                 self.straight_neighbors.append(grid[self.x + 1][self.y])
                 downOpen = True
 
         if self.y != 0:
-            if grid[self.x - 1][self.y].checkState() != "barrier":  # Up
+            if grid[self.x - 1][self.y].check_state() != "barrier":  # Up
                 self.straight_neighbors.append(grid[self.x - 1][self.y])
                 upOpen = True
 
@@ -137,24 +137,24 @@ class VisualNode:
 
         return l
 
-    def getNeighborsStraight(self, grid):
+    def get_neighbors_straight(self, grid):
         self.straight_neighbors = []
 
         # Straight Neighbors
         if self.y != GRID_X - 1:
-            if grid[self.x][self.y + 1].checkState() != "barrier":  # Right
+            if grid[self.x][self.y + 1].check_state() != "barrier":  # Right
                 self.straight_neighbors.append(grid[self.x][self.y + 1])
 
         if self.y != 0:
-            if grid[self.x][self.y - 1].checkState() != "barrier":  # Left
+            if grid[self.x][self.y - 1].check_state() != "barrier":  # Left
                 self.straight_neighbors.append(grid[self.x][self.y - 1])
 
         if self.x != GRID_Y - 1:
-            if grid[self.x + 1][self.y].checkState() != "barrier":  # Down
+            if grid[self.x + 1][self.y].check_state() != "barrier":  # Down
                 self.straight_neighbors.append(grid[self.x + 1][self.y])
 
         if self.y != 0:
-            if grid[self.x - 1][self.y].checkState() != "barrier":  # Up
+            if grid[self.x - 1][self.y].check_state() != "barrier":  # Up
                 self.straight_neighbors.append(grid[self.x - 1][self.y])
 
         l = self.straight_neighbors
@@ -163,16 +163,16 @@ class VisualNode:
 
 
 # Reset every node except start, target, and barriers
-def resetFromSearch(grid, win):
+def reset_from_search(grid, win):
     for row in grid:
         for node in row:
             if node.color == GREEN or node.color == DARK_GREEN or node.color == MAROON:
-                node.setColor(WHITE)
-                updateNode(node, win)
+                node.set_color(WHITE)
+                update_node(node, win)
 
 
 # Retrace path from parents and color them maroon
-def retracePath(self, window):
+def retrace_path(self, window):
     pathBack = []
     if self.parent:
 
@@ -184,8 +184,8 @@ def retracePath(self, window):
                     pygame.quit()
             pygame.mixer.Channel(i % 1).play(pygame.mixer.Sound(sound))
             if currentNode.color is not ORANGE and currentNode.color is not PURPLE:
-                currentNode.setColor(MAROON)
-                updateNodeWithAnimation(currentNode, window)
+                currentNode.set_color(MAROON)
+                update_node_with_animation(currentNode, window)
 
             currentNode = currentNode.parent
             pathBack.append(currentNode)
@@ -195,13 +195,13 @@ def retracePath(self, window):
 
 
 # A* pathfinding algorithm
-def astarAlgo(startNode, targetNode, grid, window):
-    resetFromSearch(grid, window)
+def a_star_algo(startNode, targetNode, grid, window):
+    reset_from_search(grid, window)
     openSet = []
     closedSet = []
     startNode.gCost = 0
     targetNode.gCost = 0
-    startNode.hCost = getDistance(startNode, targetNode)
+    startNode.hCost = get_distance(startNode, targetNode)
     targetNode.hCost = 0
 
     openSet.append(startNode)
@@ -212,47 +212,47 @@ def astarAlgo(startNode, targetNode, grid, window):
         for node in openSet:
             if node == openSet[0]:
                 continue
-            if node.getFCost() < currentNode.getFCost() or (
-                    node.getFCost() == currentNode.getFCost() and node.hCost < currentNode.hCost):
+            if node.get_f_cost() < currentNode.get_f_cost() or (
+                    node.get_f_cost() == currentNode.get_f_cost() and node.hCost < currentNode.hCost):
                 currentNode = node
 
         openSet.remove(currentNode)
         closedSet.append(currentNode)
 
         if currentNode is not startNode:
-            currentNode.setColor(GREEN)
-            updateNode(currentNode, window)
+            currentNode.set_color(GREEN)
+            update_node(currentNode, window)
 
-        for neighbor in currentNode.getNeighbors(grid):
+        for neighbor in currentNode.get_neighbors(grid):
 
             if neighbor is targetNode:
                 neighbor.parent = currentNode
                 print("Path found")
-                return retracePath(neighbor, window)
+                return retrace_path(neighbor, window)
 
-            if neighbor.checkState() != "walkable" or neighbor in closedSet:
+            if neighbor.check_state() != "walkable" or neighbor in closedSet:
                 continue
 
-            costToNeighbor = currentNode.gCost + getDistance(currentNode, neighbor)
+            costToNeighbor = currentNode.gCost + get_distance(currentNode, neighbor)
             if costToNeighbor < neighbor.gCost or neighbor not in openSet:
                 neighbor.gCost = costToNeighbor
-                neighbor.hCost = getDistance(neighbor, targetNode)
+                neighbor.hCost = get_distance(neighbor, targetNode)
                 neighbor.parent = currentNode
 
                 if neighbor not in openSet:
                     openSet.append(neighbor)
                     if neighbor is not startNode and neighbor.color is not GREEN:
                         pygame.mixer.music.play()
-                        neighbor.setColor(DARK_GREEN)
-                        updateNode(neighbor, window)
+                        neighbor.set_color(DARK_GREEN)
+                        update_node(neighbor, window)
 
     print("Path not found")
     return []
 
 
 # Breadth First Search pathfinding algorithm
-def bfsAlgo(startNode, targetNode, grid, window):
-    resetFromSearch(grid, window)
+def bfs_algo(startNode, targetNode, grid, window):
+    reset_from_search(grid, window)
     visited = []
     queue = []
     visited.append(startNode)
@@ -263,23 +263,23 @@ def bfsAlgo(startNode, targetNode, grid, window):
 
         if currentNode is not startNode:
             pygame.mixer.music.play()
-            currentNode.setColor(GREEN)
-            updateNode(currentNode, window)
+            currentNode.set_color(GREEN)
+            update_node(currentNode, window)
 
-        for neighbor in currentNode.getNeighborsStraight(grid):
+        for neighbor in currentNode.get_neighbors_straight(grid):
 
             if neighbor is targetNode:
                 neighbor.parent = currentNode
                 print("Path found")
-                return retracePath(neighbor, window)
+                return retrace_path(neighbor, window)
 
-            if neighbor.checkState() != "walkable":
+            if neighbor.check_state() != "walkable":
                 continue
 
             if neighbor is not startNode and neighbor.color is not GREEN:
                 pygame.mixer.music.play()
-                neighbor.setColor(DARK_GREEN)
-                updateNode(neighbor, window)
+                neighbor.set_color(DARK_GREEN)
+                update_node(neighbor, window)
 
             if neighbor not in visited:
                 visited.append(neighbor)
@@ -288,20 +288,20 @@ def bfsAlgo(startNode, targetNode, grid, window):
 
 
 # Update visuals of node
-def updateNode(node, window):
+def update_node(node, window):
     node.draw(window)
     pygame.display.update(node.rect)
 
 
 # Update visuals of node with an animation and threading
-def updateNodeWithAnimation(node, window):
-    d = threading.Thread(target=node.drawWithAnimationLonger(window))
+def update_node_with_animation(node, window):
+    d = threading.Thread(target=node.draw_with_longer_animation(window))
     d.start()
     pygame.display.update(node.rect)
 
 
 # Convert x, y coordinates into grid values
-def coordToGrid(coord):
+def coord_to_grid(coord):
     x, y = coord
 
     column = x // (windowWidth // GRID_X)
@@ -310,7 +310,7 @@ def coordToGrid(coord):
     return column, row
 
 
-def drawGrid(win):
+def draw_grid(win):
     grid = []
     # Populate grid
     for i in range(GRID_Y):
@@ -321,18 +321,18 @@ def drawGrid(win):
 
     # Make outline
     for node in grid[0]:
-        node.setColor(BLACK)
+        node.set_color(BLACK)
     for node in grid[GRID_Y - 1]:
-        node.setColor(BLACK)
+        node.set_color(BLACK)
     vertical1 = []
     vertical2 = []
     for list in grid:
         vertical1.append(list[0])
         vertical2.append(list[GRID_X - 1])
     for node in vertical1:
-        node.setColor(BLACK)
+        node.set_color(BLACK)
     for node in vertical2:
-        node.setColor(BLACK)
+        node.set_color(BLACK)
 
     # Draw nodes in grid
     for row in grid:
@@ -342,6 +342,7 @@ def drawGrid(win):
     pygame.display.update()
 
     return grid
+
 
 # Color constants
 WHITE = (215, 215, 215)
@@ -389,7 +390,7 @@ def main():
     pygame.mixer.music.load(sound)
     pygame.display.set_caption("Pathfinder Visualizer by Mohammad Baqer")
     window.fill(BLACK)  # Black background acts as "outline" for the nodes
-    grid = drawGrid(window)
+    grid = draw_grid(window)
 
     start = None
     target = None
@@ -401,56 +402,56 @@ def main():
 
             if pygame.mouse.get_pressed(num_buttons=3)[0]:  # Left mouse click, set nodes to start, target, or barrier
                 coord = pygame.mouse.get_pos()
-                row, column = coordToGrid(coord)
+                row, column = coord_to_grid(coord)
 
                 node = grid[column][row]
 
-                if not start and node is not target and node.checkState() == "walkable":
-                    node.setColor(PURPLE)  # Start node
+                if not start and node is not target and node.check_state() == "walkable":
+                    node.set_color(PURPLE)  # Start node
                     start = node
                     print("Start node set.")
                     time.sleep(0.1)
 
-                elif not target and node is not start and node.checkState() == "walkable":
-                    node.setColor(ORANGE)  # Target node
+                elif not target and node is not start and node.check_state() == "walkable":
+                    node.set_color(ORANGE)  # Target node
                     target = node
                     print("Target node set.")
                     time.sleep(0.1)
 
                 elif node is not start and node is not target:
-                    node.setColor(BLACK)  # Barrier node
+                    node.set_color(BLACK)  # Barrier node
 
-                updateNode(node, window)
+                update_node(node, window)
 
             elif pygame.mouse.get_pressed(num_buttons=3)[2]:  # Right mouse click, reset clicked node to walkable
                 coord = pygame.mouse.get_pos()
-                row, column = coordToGrid(coord)
+                row, column = coord_to_grid(coord)
 
                 node = grid[column][row]
-                node.setColor(WHITE)  # Walkable node color
+                node.set_color(WHITE)  # Walkable node color
 
                 if node is start:
                     start = None
                 elif node is target:
                     target = None
 
-                updateNode(node, window)
+                update_node(node, window)
 
             elif e.type == pygame.KEYDOWN:
 
                 if e.key == pygame.K_SPACE and start and target:  # A* algorithm
-                    astarAlgo(start, target, grid, window)
+                    a_star_algo(start, target, grid, window)
 
                 elif e.key == pygame.K_b and start and target:  # BFS algorithm
-                    bfsAlgo(start, target, grid, window)
+                    bfs_algo(start, target, grid, window)
 
                 elif e.key == pygame.K_x and start and target:  # Reset but keep barriers, start node, and target node
-                    resetFromSearch(grid, window)
+                    reset_from_search(grid, window)
 
                 elif e.key == pygame.K_c:  # Reset everything
                     start = None
                     target = None
-                    grid = drawGrid(window)
+                    grid = draw_grid(window)
 
 
 if __name__ == '__main__':
